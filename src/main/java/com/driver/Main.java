16e00563c6a72fe3259315ae1c828cc3bb164b7b
suryatejasunkoju <sunkojusuryateja@gmail.com>
2023-01-16 18:41:38 +0530

    done with code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    static class  Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(23, 456);
+        p.product(234, 3, 1);
+        p.product(78.98, 98.9);
+    }
 }
